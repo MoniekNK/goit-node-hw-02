6f0e913e74c5695b57cc12fa10f276545886cb4c
MoniekNK <monika.nowakowska01@gmail.com>
2024-02-16 22:41:01 +0100

    New

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-
+const Joi = require("joi");
 const contactsRouter = require("./routes/api/contacts");
 const authRouter = require("./routes/authRoutes");
 const currentUserRouter = require("./routes/currentUserRoutes");
@@ -14,7 +14,22 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
+const contactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+});
+
+const validateContact = (req, res, next) => {
+  const { error } = contactSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  next();
+};
+
+app.use("/api/contacts", validateContact, contactsRouter);
+
 app.use("/api/auth", authRouter);
 app.use("/api", currentUserRouter);
 
